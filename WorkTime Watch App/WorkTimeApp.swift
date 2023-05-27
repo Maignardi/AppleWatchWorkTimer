9ce9d41d33ab5e21882a13461d1380345ec7d535
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-26 22:18:40 -0400

    Initial Commit

---
+++ WorkTime Watch App/WorkTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTimeApp.swift
+//  WorkTime Watch App
+//
+//  Created by matheus maignardi on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkTime_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
